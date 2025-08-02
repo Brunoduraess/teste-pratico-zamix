1ec09750a833d381c62ad338455f25ed461dcbc0
BrunoDuraess <brunoduraes03@gmail.com>
2025-08-02 01:54:39 -0300

    admin routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\checkLogin;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'login'])->name('login');
 Route::post('/loginSubmit', [AuthController::class, 'loginSubmit'])->name('loginSubmit');
 Route::get('/esqueci', [AuthController::class, 'esqueci'])->name('esqueci');
 Route::post('/esqueciSubmit', [AuthController::class, 'esqueciSubmit'])->name('esqueciSubmit');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware(checkLogin::class)->group(function (){
+    Route::get('/menu', [AdminController::class, 'menu'])->name('menu');
+});
