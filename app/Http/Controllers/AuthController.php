22f341e73dbc265eb6e118bcc273a284331c054d
BrunoDuraess <brunoduraes03@gmail.com>
2025-08-02 01:54:30 -0300

    controllers

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function loginSubmit(Request $request)
+    {
+        $request->validate(
+            [
+                'email' => 'required|email',
+                'senha' =>  'required'
+            ],
+            [
+                'email.required' => 'Informe um email.',
+                'email.email' => 'Informe um email válido.',
+                'senha.required' => 'Informe uma senha.'
+            ]
+        );
+
+        $email = $request->input('email');
+        $senha = $request->input('senha');
+
+        $usuario = User::where('email', $email)
+            ->where('status', 'Ativo')
+            ->first();
+
+        if (!$usuario) {
+            return redirect()->back()->withInput()->with('loginErro', 'Email ou senha incorretos.');
+        }
+
+        if (!password_verify($senha, $usuario->senha)) {
+            return redirect()->back()->withInput()->with('loginErro', 'Email ou senha incorretos');
+        }
+
+        $usuario->ultimo_acesso = Carbon::now();
+        $usuario->save();
+
+        session([
+            'usuario' => [
+                'id' => $usuario->id,
+                'nome' => $usuario->nome,
+                'acesso' => $usuario->acesso
+            ]
+        ]);
+
+        return redirect()->route('menu');
+    }
+
+    public function esqueci()
+    {
+        return view('auth.esqueci');
+    }
+
+    public function esqueciSubmit(Request $request)
+    {
+        $request->validate(
+            [
+                'email' => 'required|email',
+                'senha' => 'required',
+                'confirma' => 'required'
+            ],
+            [
+                'email.required' => 'Informe um email.',
+                'email.email' => 'Informe um email válido.',
+                'senha.required' => 'Informe a nova senha.',
+                'confirma.required' => 'Confirme a nova senha.'
+            ]
+        );
+
+        $email = $request->input('email');
+        $senha = $request->input('senha');
+        $confirma = $request->input('confirma');
+
+        $usuario = User::where('email', $email)
+            ->where('status', 'Ativo')
+            ->first();
+
+        if (!$usuario) {
+            return redirect()->back()->withInput()->with('loginErro', 'Usuário não encontrado.');
+        }
+
+        if ($senha != $confirma) {
+            return redirect()->back()->withInput()->with('loginErro', 'As senhas não são iguais.');
+        }
+
+        $usuario->senha = bcrypt($senha);
+        $usuario->save();
+
+        return redirect()->route('login');
+    }
+
+    public function logout()
+    {
+        session()->flush();
+        return redirect()->route('login');
+    }
+}
