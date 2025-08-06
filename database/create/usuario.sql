33042d440179dc8ca976e4e40aa64e372604d322
BrunoDuraess <brunoduraes03@gmail.com>
2025-08-06 12:14:33 -0300

    Código para criar usuário manualmente

---
+++ database/create/usuario.sql
@@ -0,0 +1,9 @@
+INSERT INTO usuarios (id, nome, email, departamento, perfil, senha) 
+VALUES (
+  '1bd13a12-485d-48de-ba19-cd54adfe0d96', 
+  'José Ninguém', 
+  'jose@zamix.com.br', 
+  'Almoxarifado', 
+  'Administrador', 
+  '$2y$12$nGiZZTuguFU51iXuOibERugQ1lMnfcErEW3r8/Y4q/YqxXwPRtsj6'
+);
